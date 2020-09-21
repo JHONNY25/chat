4bd0e63dbed7f205964888eb07e1836aaca3d165
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-09-20 20:19:53 -0600

    update components jetstream

---
+++ resources/views/Components/authentication-card.blade.php
@@ -0,0 +1,8 @@
+<div class="flex sm:justify-center items-center pt-6 sm:pt-0" style="width: 400px;">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden rounded bg-gray-800 ">
+        <div class="mb-10 flex justify-center">
+            {{ $logo }}
+        </div>
+        {{ $slot }}
+    </div>
+</div>
