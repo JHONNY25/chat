a1c8f066b7a7f2708c5873fd61bcaf9bf1418819
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-09-23 19:51:43 -0600

    remove class bg-white

--- resources/views/Components/authentication-card.blade.php
+++ resources/views/Components/authentication-card.blade.php
@@ -1,5 +1,5 @@
 <div class="flex sm:justify-center items-center pt-6 sm:pt-0" style="width: 400px;">
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden rounded bg-gray-800 ">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden rounded bg-gray-800 ">
         <div class="mb-10 flex justify-center">
             {{ $logo }}
         </div>
