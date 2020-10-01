dfaa1e5cbc275ab71c29336c2ac3dff05b27dd0f
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-09-30 19:21:49 -0600

    create component search input

---
+++ resources/views/livewire/search-input.blade.php
@@ -0,0 +1,13 @@
+<div class="border-b border-gray-700">
+    <div class="my-3 mx-3 ">
+        <div class="relative text-gray-600 focus-within:text-gray-400">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                <button type="submit" class="p-1 outline-none focus:outline-none focus:shadow-outline">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                </button>
+              </span>
+            <input wire:model="search" aria-placeholder="Busca tus amigos o contacta nuevos" placeholder="Busca tus amigos o contacta nuevos"
+            class="py-2 pl-10 block w-full rounded-full bg-gray-900 outline-none focus:outline-none focus:bg-white focus:text-gray-900" type="search" name="search" required autocomplete="search" />
+        </div>
+    </div>
+</div>
