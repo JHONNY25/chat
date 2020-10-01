6512cbe7faaad2af693429a4fe5641a7c80823c9
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-09-30 19:19:53 -0600

    change layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,28 +14,21 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         @livewireStyles
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.6.0/dist/alpine.js" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="flex justify-center min-h-screen bg-gray-900 sm:items-center sm:pt-0">
-            {{-- @livewire('navigation-dropdown') --}}
-            @if (Route::has('login'))
-                @guest
-                    @if (Route::has('register'))
-                        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        </div>
-                    @endif
-                @endguest
-            @endif
-
             <!-- Page Content -->
             <main class="min-h-screen min-w-full">
-                {{ $slot }}
+                <div class="lg:w-full xl:w-9/12 mx-auto my-auto" style="min-height: 75% !important">
+                    <div class="overflow-hidden shadow-xl rounded-none min-h-screen">
+                        {{ $slot }}
+                    </div>
+                </div>
             </main>
-
         </div>
 
         @stack('modals')
