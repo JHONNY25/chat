287830585a1483b15c5f88e6d77fd998fd2a1ee9
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-16 14:58:10 -0600

    add relationships

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Chat extends Model
 {
     use HasFactory;
+    
+    protected $table = 'chats';
+    protected $fillable = [
+        'user_recive',
+        'user_sent',
+        'text',
+        'image_path',
+        'file_path',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -57,4 +57,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function chat(){
+        return $this->hasMany(Chat::class,'user_sent');
+    }
 }
