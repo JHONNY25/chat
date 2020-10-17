5beb1b90252cffed4c3997973565290ce2d40506
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-16 19:50:53 -0600

    update for exists chats

--- database/migrations/2020_09_17_000212_create_chats_table.php
+++ database/migrations/2020_09_17_000212_create_chats_table.php
@@ -15,11 +15,8 @@ class CreateChatsTable extends Migration
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_recive');
             $table->unsignedBigInteger('user_sent');
-            $table->text('text');
-            $table->string('image_path')->nullable();
-            $table->string('file_path')->nullable();
+            $table->unsignedBigInteger('user_recive');
             $table->timestamps();
 
             $table->foreign('user_recive')->references('id')->on('users');
