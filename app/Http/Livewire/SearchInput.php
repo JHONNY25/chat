dfaa9ab229870c01c5939d0f49de98a6fd1fed9e
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-25 19:36:55 -0700

    add users and conditions for get users

--- app/Http/Livewire/SearchInput.php
+++ app/Http/Livewire/SearchInput.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class SearchInput extends Component
 {
     public $search;
+    public $users;
 
     public function mount(){
         $this->search = '';
+        $this->users = [];
     }
 
     public function render()
     {
+        if(strlen($this->search) > 0){
+            if(User::where('name','like','%'.$this->search.'%')->exists()){
+                $this->users = User::where('name','like','%'.$this->search.'%')->get();
+            }
+        }else{
+            $this->users = [];
+        }
+
         return view('livewire.search-input');
     }
 }
