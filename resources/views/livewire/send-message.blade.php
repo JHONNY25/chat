6c7bda43e4bf6971b3e81bf3b74441d16f7a40ed
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-10-28 15:12:42 -0700

    if text is empty not show btn

--- resources/views/livewire/send-message.blade.php
+++ resources/views/livewire/send-message.blade.php
@@ -6,11 +6,12 @@
     </button>
     <input wire:model="text" aria-placeholder="Escribe un mensaje aquí" placeholder="Escribe un mensaje aquí"
         class="py-2 mx-3 pl-5 block w-full rounded-full bg-gray-900 outline-none focus:outline-none focus:bg-white focus:text-gray-900" type="text" name="message" required/>
-
-    <button wire:click="sendMessage" class="{{ empty($text) ? 'hidden' : 'block' }} outline-none focus:outline-none">
-        <svg class="text-gray-900 h-7 w-7 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
-        </svg>
-    </button>
+    @if(!empty($text))
+        <button wire:click="sendMessage" class="outline-none focus:outline-none">
+            <svg class="text-gray-900 h-7 w-7 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+            </svg>
+        </button>
+    @endif
 </div>
 
