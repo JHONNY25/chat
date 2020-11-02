b2d3675ab55507142be030028f22a0d3868a514c
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-11-01 20:00:21 -0700

    create event writing for show if user is wiritng

---
+++ app/Events/Writing.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Writing implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user_name;
+    public $user_current;
+
+    public function __construct($user_name = null,$user_current = null)
+    {
+        $this->user_name = $user_name;
+        $this->user_current = $user_current;
+    }
+
+    public function broadcastOn()
+    {
+        return ['livechat-channel'];
+    }
+
+    public function broadcastAs()
+    {
+        return 'writing';
+    }
+}
