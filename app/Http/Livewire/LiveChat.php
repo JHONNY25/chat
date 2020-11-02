e16fcb1b61d6130a6478f26fc2ba8897680cc68f
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-11-01 20:05:35 -0700

    add listener userWriting for set $data or not

--- app/Http/Livewire/LiveChat.php
+++ app/Http/Livewire/LiveChat.php
@@ -12,17 +12,28 @@ class LiveChat extends Component
     public $userchatid;
     public $messages;
     public $usercurrent;
+    public $userWriting;
 
     protected $listeners = [
         'messageSent' => 'refresh',
         'reciveMessage' => 'refresh',
+        'userWriting' => 'userWriting',
     ];
+
+    public function mount($id){
+        $this->usercurrent = Auth::user();
+        $this->userchatid = $id;
+        $this->userWriting = [];
+    }
 
     public function refresh(){ }
 
-    public function mount($id){
-        $this->usercurrent = Auth::id();
-        $this->userchatid = $id;
+    public function userWriting($data){
+        if($data !== null){
+            $this->userWriting  = $data;
+        }else{
+            $this->userWriting = [];
+        }
     }
 
     public function render()
